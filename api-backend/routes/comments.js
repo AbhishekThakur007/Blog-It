7118f28de02f7ffafa1a1e13ead28dda7e0a35ec
Abhishek Thakur <abhishekthakur_01@yahoo.com>
2024-09-15 16:29:55 +0530

    Adding Remaining Routes for Comments and Notification

---
+++ api-backend/routes/comments.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const verifyToken = require('../middleware/verifyToken');
+
+const { doGetComments,
+        doAddComment, 
+        doDeleteComment, 
+        doEditComment } = require('../controllers/comment-controllers');
+
+router.route('/getAll/:id').get(doGetComments);
+router.route('/addComment/:id').post(verifyToken, doAddComment);
+router.route('/:id').put(verifyToken, doEditComment).delete(verifyToken, doDeleteComment);
+
+module.exports = router;
+ 
